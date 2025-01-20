6a05aa198d8405b40d07fa8959e494b3765622a6
MihaiElisei <mihai.m.elisei@gmail.com>
2025-01-20 13:09:52 +0200

    added configuration for keycloak

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, provideAppInitializer, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { inject } from '@angular/core';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import Keycloak from 'keycloak-js';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAppInitializer(() => {
+      const initFn = ((key:Keycloak) => {
+        return () => key.init()
+      }) (inject(Keycloak));
+      return initFn();
+    })
     ]
 };
